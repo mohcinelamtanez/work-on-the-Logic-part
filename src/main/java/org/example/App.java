ae55bda587c54da1e684b1ae489c2a5c82ea59a9
mohcinelamtanez <lamtanezmohcine95@gmail.com>
2026-07-08 23:50:46 +0100

    testing my solution

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
+        int nums[] = {2 , 4 , 4 , 6 , 10} ;
+        System.out.println(Solution1.maxSumSubArrayOfSizek(nums , 3)) ;
     }
 }
